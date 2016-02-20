c6f5ac415c555120e4c8b7f0339baad2b166d72c
JGaleazzi <jacqueline.galeazzi@student.uhasselt.be>
2016-02-20 20:13:44 +0100

    Portfolio Jacqueline Galeazzi
    Portfolio Jacqueline Galeazzi

---
+++ High_Fidelity_proptype/files/results/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n,o,p,q],r,_(s,t,u,v,w,x,y,_(),z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,D,L,D,M,N,O,null,P,Q,R,S,T,U,V,Q),W,_(X,_()),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
+var b="url",c="results.html",d="generationDate",e=new Date(1452799166035.06),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="ColorWordsVar",k="oddOneOut",l="wordCounting",m="wordColors",n="listingLetters",o="listingCategories",p="warmUp",q="CurrentExercise",r="page",s="packageId",t="850a21e5edfc4e8e9982d498c83eed51",u="type",v="Axure:Page",w="name",x="Results",y="notes",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFFFFFFFF,J="image",K="imageHorizontalAlignment",L="imageVerticalAlignment",M="imageRepeat",N="auto",O="favicon",P="sketchFactor",Q="0",R="colorStyle",S="appliedColor",T="fontName",U="Applied Font",V="borderWidth",W="adaptiveStyles",X="a",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
+return _creator();
+})());
